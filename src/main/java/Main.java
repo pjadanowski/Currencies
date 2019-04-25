564377458b2a9b8a69217761f08a8715b2ba1ada
Paweł Jadanowski <pjadanowski@gmail.com>
2019-04-25 23:03:43 +0200

    pobieranie hrefa z currency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.gargoylesoftware.htmlunit.html.DomNode;
 import com.gargoylesoftware.htmlunit.html.DomNodeList;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.w3c.dom.NamedNodeMap;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,9 +35,59 @@ public class Main {
             theadColumns.add(child.asText());
         }
 
-        DomNodeList<DomNode> tr = table.querySelectorAll("tr");
+        DomNodeList<DomNode> trList = table.querySelectorAll("tr");
 
-        System.out.println(theadColumns);
+
+        for (DomNode trNode: trList) {
+            System.out.println("TR: "+trNode.getIndex());
+
+            // zobaczmy ile td'ków ma row
+            String[] split = trNode.asText().split("\t");
+
+
+            DomNodeList<DomNode> tds = trNode.querySelectorAll("td");
+
+            for (DomNode td: tds) {
+                // sprawdz czy td at index 1 hasRowSpan=...
+//                int rowspan = 0;
+//                if (td.getIndex() == 1) {
+//                    NamedNodeMap attributes = td.getAttributes();
+//                    if (attributes.getNamedItem("rowspan") != null) {
+//                        rowspan = Integer.parseInt(attributes.getNamedItem("rowspan").getNodeValue());
+//                        System.out.println("mamy rowspan: " + attributes.getNamedItem("rowspan").getNodeValue());
+//                    }
+//                }
+                String tdText = td.asText();
+                System.out.print("\t\ttd:"+td.getIndex() + " " + tdText + "\t");
+
+                if ( split.length == 6 && td.getIndex() == 3) {
+
+                    // pobierz href z 3
+                    DomNode a = td.querySelector("a");
+                    if (a == null) continue;
+                    NamedNodeMap a_attributes = a.getAttributes();
+                    if (a_attributes.getNamedItem("href") != null) {
+                        String href = a_attributes.getNamedItem("href").getNodeValue();
+                        System.out.print(href + "\t\t");
+                    }
+                }
+
+                if (split.length == 5 && td.getIndex() == 1) {
+                    // pobierz href z 1
+                    DomNode a = td.querySelector("a");
+                    if (a == null) continue;
+                    NamedNodeMap a_attributes = a.getAttributes();
+                    if (a_attributes.getNamedItem("href") != null) {
+                        String href = a_attributes.getNamedItem("href").getNodeValue();
+                        System.out.print(href + "\t\t");
+                    }
+                }
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
 
     }
 }
